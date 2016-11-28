b8e5f8b65ee9ba3ac86de9da5aec0fd080dd0780
nikojpapa <nicholas.papadopoulos@comcast.net>
2016-11-27 23:09:29 -0500

    firebase anonymous sign in

--- static/script/index.js
+++ static/script/index.js
@@ -34,12 +34,12 @@ firebase.auth().onAuthStateChanged(function(user) {
   // ...
 });
 
-// firebase.auth().signInAnonymously().catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // ...
-// });
+firebase.auth().signInAnonymously().catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
 
 function isEmpty(abc) {
     for(var prop in abc) {
